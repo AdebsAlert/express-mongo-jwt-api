0f290cae355d08cf42477eaf1eeac850aaa411cb
Adebayo Mustafa <adebsalert@gmail.com>
2019-06-08 17:36:54 +0100

    changed to findOneAndUpdate

--- src/controllers/user/delete.js
+++ src/controllers/user/delete.js
@@ -5,8 +5,8 @@ const httpStatus = require('http-status')
 
 exports.deleteUser = async (req, res, next) => {
   try {
-    const deleted = await User.remove({_id: req.params.user})
-    if (deleted.n === 0 || deleted === null) {
+    const deleted = await User.findOneAndRemove({_id: req.params.user})
+    if (deleted === null) {
       res.status(httpStatus.BAD_REQUEST)
       res.send({ success: false, message: 'cannot delete user', deleted })
     }
--- src/controllers/user/update.js
+++ src/controllers/user/update.js
@@ -5,15 +5,15 @@ const httpStatus = require('http-status')
 
 exports.updateUser = async (req, res, next) => {
   try {
-    await User.updateOne({_id: req.params.user}, req.body, (error, user) => {
-      if (error) {
+    await User.findOneAndUpdate({_id: req.params.user}, req.body, {new: true}, (error, user) => {
+      if (error || user === null) {
         res.status(httpStatus.BAD_REQUEST)
         res.send({ success: false, message: 'cannot update user', error })
       }
 
       if (user) {
         res.status(httpStatus.CREATED)
-        res.send({ success: true, message: 'user updated successfully', data: req.body })
+        res.send({ success: true, message: 'user updated successfully', data: user })
       }
     })
   } catch (error) {
